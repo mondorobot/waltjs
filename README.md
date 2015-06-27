2bdf4ac58e39433b9a96e52f301a0ae7b5699a97
Andy Mikulski <andy.mikulski@gmail.com>
2015-06-27 16:41:05 -0600

    Refactored Walt to allow composable animations

--- README.md
+++ README.md
@@ -1,32 +1,67 @@
 # WaltJS
-## CSS3 Animation Manager
+## Composable CSS3 Animation Manager
 
-JavaScript wrapper for CSS3 animation events. Combine with Animate.css for tons of fun!
+JavaScript wrapper for CSS3 animation events. Allows for composing/forking animations on the fly. Combine with Animate.css for tons of fun!
 
-### Usage
+### Dependencies
+Requires jQuery and caolan/async
 
 ```
-Walt.animate({
-  'el': $('#yourelement'),
-  'animation': 'fadeInUp'
+// define a precomposed animation
+// note - no target is applied yet
+var intro = new Walt().name('fadeIn').duration(2500);
+
+// you can 'fork' animations to create variations of existing compositions,
+// and use `.target` to set the element(s) to animate
+var intro2 = intro.fork().target(document.body);
+
+// apply an existing animation to multiple elements
+// each element gets its own animation instance so we can play with the delay etc
+$('.stuff').each(function(i, v) {
+  intro.fork().target(v).delay(i * 50).animate();
 });
-```
 
-Available options:
+// if we didn't want any per-instance property changes,
+// e.g. to animate a set of things at once,
+// we can just change the target to collect multiples
+//   (still forking as we want to extend an existing composition,
+//    if we didn't fork then we'd be altering the 'master' copy)
+intro.fork().target('.stuff').animate();
 
-```
-interface WaltOptions {
-  el:any;
-  animation:string;
-  delay?:string;
-  duration?:string;
-  direction?:string;
-  timing?:string;
-  fill?:string;
-  count?:string;
-  onComplete?:any;
-  onBefore?:any;
-  fallback?:any;
-  useTimeout?:boolean;
-}
+
+// chain animations via `then` / `after`
+intro.then(intro2.animate.bind(intro2)).animate();
+
+
+// available commands:
+new Walt()
+  // animations have default settings, but can be overridden
+  // in the following fashion:
+
+  .target(element) // element to run animation on
+  .name('animationName') // name of the CSS animation to apply
+  .duration(1000) // animation speed (in ms)
+  .delay(1000) // animation delay (in ms)
+  .count(3) // how many anims to play (default 1)
+  .fill('forwards') // fill mode 
+  .timing('cubic-bezier(0.86, 0, 0.07, 1)')
+  // walt also provides timing easings
+  .timing(Walt.prototype.easings.easeInOutQuint)
+
+  // there are some control functions
+  .pause() // pauses animation
+  .resume() // resumes/plays an animation
+  .play() // resumes/plays an animation
+
+  // some functions are great for extending base compositions
+  .children() // select the current target's children
+  .fork() // ** clone animation definition with current settings **
+
+  // you can also add before/after functions to fire before/after the animation
+  .before(function($el, settings){ console.log('before anim starts'); })
+  .after(function($el, settings){ console.log('after anim ends'); })
+  // you can specify 'each' commands if you're animating a collection of elements
+  // and want a handler per-item
+  .beforeEach(function($el, settings){ console.log('before each individual anim starts'); })
+  .afterEach(function($el, settings){ console.log('after each individual anim ends'); })
 ```
---
+++ walt.js
@@ -0,0 +1,323 @@
+/*!
+ * WaltJS
+ * @author Andy Mikulski <andy@mondorobot.com>
+ */
+;
+(function(window, document, $, async, undefined) {
+
+
+  // object to track existing animation
+  // behaves an animation stream basically
+  window.Walt = (function(Walt) {
+    function Walt(options) {
+      var anim = this;
+
+      anim.settings = anim.defaults;
+
+      // for grouped animations
+      anim.onBefores = [];
+      anim.onAfters = [];
+
+      // for animation items
+      anim.onBeforeEaches = [];
+      anim.onAfterEaches = [];
+
+      return anim;
+    }
+
+    function convertToCSSTime(value) {
+      if (value.indexOf && (value.indexOf('ms') > -1 || value.indexOf('s') > -1)) {
+        // sall good
+      } else {
+        value = value + 'ms';
+      }
+      return value;
+    }
+
+    Walt.prototype = {
+      'defaults': {
+        'element': $(document.body || document.documentElement),
+        'animation': 'fadeIn',
+        'delay': '0s',
+        'duration': '1s',
+        'count': 1,
+        'fill': 'both',
+        'direction': 'normal',
+        'timing': 'ease',
+        'state': 'paused'
+      },
+
+      'fork': function() {
+        var anim = this;
+
+        // would like to find a non-jquery deep clone
+        return $.extend(true, Object.create(Walt), anim);
+      },
+
+
+      // mutation functions
+      'target': function($el) {
+        var anim = this;
+
+        $el = !($el instanceof $) ? $($el) : $el;
+
+        anim.settings.element = $el;
+
+        return anim;
+      },
+
+      'children': function(selector) {
+        var anim = this;
+
+        if (selector) {
+          anim.settings.element = anim.settings.element.find(selector);
+        } else {
+          anim.settings.element = anim.settings.element.children();
+        }
+
+        return anim;
+      },
+
+      'name': function(name) {
+        var anim = this;
+
+        anim.animation = name;
+
+        return anim;
+      },
+
+      'fill': function(value) {
+        var anim = this;
+
+        anim.fill = value;
+
+        return anim;
+      },
+
+      'direction': function(value) {
+        var anim = this;
+
+        anim.direction = value;
+
+        return anim;
+      },
+
+      'timing': function(value) {
+        var anim = this;
+
+        anim.timing = value;
+
+        return anim;
+      },
+
+      'duration': function(value) {
+        var anim = this;
+
+        value = convertToCSSTime(value);
+        anim.settings.duration = value;
+
+        return anim;
+      },
+      'delay': function(value) {
+        var anim = this;
+
+        value = convertToCSSTime(value);
+        anim.settings.delay = value;
+
+        return anim;
+      },
+      'count': function(value) {
+        var anim = this;
+
+        anim.settings.count = value;
+
+        return anim;
+      },
+      'before': function(onBeforeFnc) {
+        var anim = this;
+
+        anim.onBefores.push(function(callback) {
+          onBeforeFnc && onBeforeFnc(anim.settings.element, anim.settings);
+          callback && callback(null, null);
+        });
+
+        return anim;
+      },
+      'beforeEach': function(onBeforeEachFnc) {
+        var anim = this;
+
+        anim.onBeforeEaches.push(function(callback) {
+          onBeforeEachFnc && onBeforeEachFnc(anim.settings.element, anim.settings);
+          callback && callback(null, null);
+        });
+
+        return anim;
+      },
+
+
+      'after': function(onAfterFnc) {
+        var anim = this;
+
+        anim.onAfters.push(function(callback) {
+          onAfterFnc && onAfterFnc(anim.settings.element, anim.settings);
+          callback && callback(null, null);
+        });
+
+        return anim;
+      },
+      'then': function(thenFnc) {
+        var anim = this;
+
+        return anim.after(thenFnc);
+      },
+      'afterEach': function(onAfterEachFnc) {
+        var anim = this;
+
+        anim.onAfterEaches.push(function(callback) {
+          onAfterEachFnc && onAfterEachFnc(anim.settings.element, anim.settings);
+          callback && callback(null, null);
+        });
+
+        return anim;
+      },
+
+
+      'done': function(doneFnc) {
+        var anim = this;
+
+        return anim.after(doneFnc);
+      },
+
+      'pause': function() {
+        var anim = this;
+
+        anim.settings.state = 'paused';
+
+        return anim;
+      },
+
+      'play': function() {
+        var anim = this;
+
+        anim.settings.state = 'running';
+
+        return anim;
+      },
+
+      'resume': function() {
+        var anim = this;
+
+        anim.settings.state = 'running';
+
+        return anim;
+      },
+
+
+
+      // functional methods
+
+      'animate': function() {
+        var anim = this;
+
+        async.parallel(anim.onBefores, anim._executeAnim.bind(anim));
+
+        return anim;
+      },
+
+      '_createCssObj': function(cssString) {
+        var anim = this;
+
+        return {
+          '-webkit-animation': cssString || '',
+          'animation': cssString || ''
+        };
+      },
+
+
+      '_executeAnim': function() {
+        var anim = this;
+        // modify the animation before we build the CSS string
+        anim.resume();
+
+        var settings = anim.settings,
+          cssString = settings.animation + ' ' + settings.duration + ' ' + settings.timing + ' ' + settings.delay + ' ' + settings.count + ' ' + settings.direction + ' ' + settings.fill + ' ' + settings.state;
+
+        // execution = we're playing
+
+        anim.animCount = 0;
+        anim.animMax = anim.settings.element.length;
+
+        anim.settings.element.on('onanimationend animationend', anim._onAnimEndEvent.bind(anim));
+        anim.settings.element.css(anim._createCssObj(cssString));
+      },
+
+      '_onAnimEndEvent': function(event) {
+        var anim = this,
+          $target = $(event.currentTarget);
+
+        anim.animCount += 1;
+
+        // fire afterEach for each one
+
+        async.parallel(anim.onAfterEaches, function() {
+          // if we're done then we can fire the onAfters
+          if (anim.animCount >= anim.animMax) {
+            anim._onAnimComplete();
+          }
+        });
+      },
+
+      '_onAnimComplete': function() {
+        var anim = this;
+
+        async.parallel(anim.onAfters, function() {
+          delete anim.animCount;
+          delete anim.animMax;
+
+          // reset the css by just passing in null values for the properties
+          anim.settings.element.css(anim._createCssObj());
+        });
+      },
+
+
+      'easings': {
+        'linear': 'cubic-bezier(0.250, 0.250, 0.750, 0.750)',
+        'ease': 'cubic-bezier(0.250, 0.100, 0.250, 1.000)',
+        'easeIn': 'cubic-bezier(0.420, 0.000, 1.000, 1.000)',
+        'easeOut': 'cubic-bezier(0.000, 0.000, 0.580, 1.000)',
+        'easeInOut': 'cubic-bezier(0.420, 0.000, 0.580, 1.000)',
+
+        'easeInQuad': 'cubic-bezier(0.550, 0.085, 0.680, 0.530)',
+        'easeInCubic': 'cubic-bezier(0.550, 0.055, 0.675, 0.190)',
+        'easeInQuart': 'cubic-bezier(0.895, 0.030, 0.685, 0.220)',
+        'easeInQuint': 'cubic-bezier(0.755, 0.050, 0.855, 0.060)',
+        'easeInSine': 'cubic-bezier(0.470, 0.000, 0.745, 0.715)',
+        'easeInExpo': 'cubic-bezier(0.950, 0.050, 0.795, 0.035)',
+        'easeInCirc': 'cubic-bezier(0.600, 0.040, 0.980, 0.335)',
+        'easeInBack': 'cubic-bezier(0.600, -0.280, 0.735, 0.045)',
+
+        'easeOutQuad': 'cubic-bezier(0.250, 0.460, 0.450, 0.940)',
+        'easeOutCubic': 'cubic-bezier(0.215, 0.610, 0.355, 1.000)',
+        'easeOutQuart': 'cubic-bezier(0.165, 0.840, 0.440, 1.000)',
+        'easeOutQuint': 'cubic-bezier(0.230, 1.000, 0.320, 1.000)',
+        'easeOutSine': 'cubic-bezier(0.390, 0.575, 0.565, 1.000)',
+        'easeOutExpo': 'cubic-bezier(0.190, 1.000, 0.220, 1.000)',
+        'easeOutCirc': 'cubic-bezier(0.075, 0.820, 0.165, 1.000)',
+        'easeOutBack': 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
+
+        'easeInOutQuad': 'cubic-bezier(0.455, 0.030, 0.515, 0.955)',
+        'easeInOutCubic': 'cubic-bezier(0.645, 0.045, 0.355, 1.000)',
+        'easeInOutQuart': 'cubic-bezier(0.770, 0.000, 0.175, 1.000)',
+        'easeInOutQuint': 'cubic-bezier(0.860, 0.000, 0.070, 1.000)',
+        'easeInOutSine': 'cubic-bezier(0.445, 0.050, 0.550, 0.950)',
+        'easeInOutExpo': 'cubic-bezier(1.000, 0.000, 0.000, 1.000)',
+        'easeInOutCirc': 'cubic-bezier(0.785, 0.135, 0.150, 0.860)',
+        'easeInOutBack': 'cubic-bezier(0.680, -0.550, 0.265, 1.550)'
+      }
+    };
+
+    return Walt;
+  })(window.Walt || {});
+
+
+})(window, document, jQuery, async);
