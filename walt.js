85c6b65bf10d1186526c1df0cabf87918008d76e
Andy Mikulski <andy.mikulski@gmail.com>
2015-06-27 21:25:24 -0600

    Added comments, re-minified

--- walt.js
+++ walt.js
@@ -5,27 +5,46 @@
 ;
 (function(window, document, $, async, undefined) {
 
-
-  // object to track existing animation
-  // behaves an animation stream basically
+  /**
+   * The Walt object.
+   * Each animation has a Walt instance associated with it,
+   * containing properties for that animation definition.
+   */
   window.Walt = (function(Walt) {
-    function Walt(options) {
+
+    /**
+     * Constructor
+     */
+    function Walt() {
       var anim = this;
 
+      // we'll apply the default options to start
       anim.settings = anim.defaults;
 
       // for grouped animations
+      // (these fire before/after full animations are done)
       anim.onBefores = [];
       anim.onAfters = [];
 
       // for animation items
+      // (these are fired before/after each animation item is done)
+      // ((this is primarily for children animations))
       anim.onBeforeEaches = [];
       anim.onAfterEaches = [];
 
       return anim;
     }
 
+    /**
+     * Utility function to convert a numeric value to a milisecond string
+     * 
+     * @param  {number} CSS Time value (delay/duration) to parse
+     * @return {string} CSS-ified time value
+     */
     function convertToCSSTime(value) {
+      // check for ms, then s, and if those are found then we already have a value
+      // that's in css time format
+      // TODO: be more accurate in checking for placement of ms/s
       if (value.indexOf && (value.indexOf('ms') > -1 || value.indexOf('s') > -1)) {
         // sall good
       } else {
@@ -34,7 +53,24 @@
       return value;
     }
 
+
+    /**
+     * Utility function to generate an animation 'css object' based on an input string
+     * @param  {string}   CSS Animation shorthand CSS to apply
+     * @return {object}   Compiled 'css object'
+     */
+    function _createCssObj(cssString) {
+      var anim = this;
+
+      return {
+        '-webkit-animation': cssString || '',
+        'animation': cssString || ''
+      };
+    }
+
+    // Walt prototype definition
     Walt.prototype = {
+      // default animation settings
       'defaults': {
         'element': $(document.body || document.documentElement),
         'animation': 'fadeIn',
@@ -44,22 +80,31 @@
         'fill': 'both',
         'direction': 'normal',
         'timing': 'ease',
+        // they are paused to start for greater control;
+        // they only begin once walt calls `resume` when its ready
         'state': 'paused'
       },
 
-      'fork': function() {
-        var anim = this;
-
-        var newGuy = new Walt();
-        for (var prop in anim) {
-          newGuy[prop] = anim[prop];
-        }
-
-        return newGuy;
-      },
 
 
-      // mutation functions
+
+      // 
+      // 
+      // 
+      // Transformation functions
+      // 
+      // 
+      // 
+
+
+
+
+      /**
+       * Sets the current animation's element target
+       * 
+       * @param  {jQuery|Element}   Element to apply the animation to
+       * @return {Walt}             This Walt animation instance
+       */
       'target': function($el) {
         var anim = this;
 
@@ -70,6 +115,15 @@
         return anim;
       },
 
+
+      /**
+       * Sets the  animation's target to the current target's children.
+       * An optional string selector may be passed in to target specific children,
+       * else $.children() is used to grab the first-level children.
+       * 
+       * @param  {string}   Optional selector string to target specific children in target
+       * @return {Walt}     This Walt animation instance
+       */
       'children': function(selector) {
         var anim = this;
 
@@ -82,6 +136,13 @@
         return anim;
       },
 
+
+      /**
+       * Set the name of the CSS animation to apply to target.
+       * 
+       * @param  {string}   Name of CSS Animation
+       * @return {Walt}     This Walt animation instance
+       */
       'name': function(name) {
         var anim = this;
 
@@ -90,6 +151,13 @@
         return anim;
       },
 
+
+      /**
+       * Set the fill mode of the CSS animation to apply to target.
+       * 
+       * @param  {string}   CSS Fill mode to apply
+       * @return {Walt}     This Walt animation instance
+       */
       'fill': function(value) {
         var anim = this;
 
@@ -98,6 +166,13 @@
         return anim;
       },
 
+
+      /**
+       * Set the direction of the CSS animation to apply to target.
+       * 
+       * @param  {string}   CSS direction to apply
+       * @return {Walt}     This Walt animation instance
+       */
       'direction': function(value) {
         var anim = this;
 
@@ -106,6 +181,13 @@
         return anim;
       },
 
+
+      /**
+       * Set the direction of the CSS animation to apply to target.
+       * 
+       * @param  {string}   CSS direction to apply
+       * @return {Walt}     This Walt animation instance
+       */
       'timing': function(value) {
         var anim = this;
 
@@ -114,22 +196,41 @@
         return anim;
       },
 
+
+      /**
+       * Set the duration of the CSS animation to apply to target.
+       * 
+       * @param  {number|string}   Duration animation should last
+       * @return {Walt}            This Walt animation instance
+       */
       'duration': function(value) {
         var anim = this;
 
-        value = convertToCSSTime(value);
-        anim.settings.duration = value;
+        anim.settings.duration = convertToCSSTime(value);
 
         return anim;
       },
+
+      /**
+       * Set the delay of the CSS animation to apply to target.
+       * 
+       * @param  {number|string}   Delay before animation should fire
+       * @return {Walt}     This Walt animation instance
+       */
       'delay': function(value) {
         var anim = this;
 
-        value = convertToCSSTime(value);
-        anim.settings.delay = value;
+        anim.settings.delay = convertToCSSTime(value);
 
         return anim;
       },
+
+      /**
+       * Set the number of times the animation should play.
+       * 
+       * @param  {number}   Number of times animation should loop before 'done'
+       * @return {Walt}     This Walt animation instance
+       */
       'count': function(value) {
         var anim = this;
 
@@ -137,9 +238,31 @@
 
         return anim;
       },
+
+
+
+
+      // 
+      // 
+      // 
+      // Event handling functions
+      // 
+      // 
+      // 
+
+
+
+
+      /**
+       * Add function to fire just before the animation takes place
+       * 
+       * @param  {function}   onBefore Function
+       * @return {Walt}       This Walt animation instance
+       */
       'before': function(onBeforeFnc) {
         var anim = this;
 
+        // gotta wrap it in a callback thing since we'll async it later
         anim.onBefores.push(function(callback) {
           onBeforeFnc && onBeforeFnc(anim.settings.element, anim.settings);
           callback && callback(null, null);
@@ -147,9 +270,18 @@
 
         return anim;
       },
+
+      /**
+       * Add function to fire just before each animation item takes place
+       * (This is pretty much for animation a collection of items)
+       * 
+       * @param  {function}   onBeforeEach Function
+       * @return {Walt}       This Walt animation instance
+       */
       'beforeEach': function(onBeforeEachFnc) {
         var anim = this;
 
+        // gotta wrap it in a callback thing since we'll async it later
         anim.onBeforeEaches.push(function(callback) {
           onBeforeEachFnc && onBeforeEachFnc(anim.settings.element, anim.settings);
           callback && callback(null, null);
@@ -159,9 +291,16 @@
       },
 
 
+      /**
+       * Add function to fire just after the animation takes place
+       * 
+       * @param  {function}   onAfter Function
+       * @return {Walt}       This Walt animation instance
+       */
       'after': function(onAfterFnc) {
         var anim = this;
 
+        // gotta wrap it in a callback thing since we'll async it later
         anim.onAfters.push(function(callback) {
           onAfterFnc && onAfterFnc(anim.settings.element, anim.settings);
           callback && callback(null, null);
@@ -169,14 +308,42 @@
 
         return anim;
       },
+
+      /**
+       * `after` alias
+       * 
+       * @param  {function}   onAfter Function
+       * @return {Walt}       This Walt animation instance
+       */
       'then': function(thenFnc) {
         var anim = this;
 
         return anim.after(thenFnc);
       },
+
+      /**
+       * `after` alias
+       * 
+       * @param  {function}   onAfter Function
+       * @return {Walt}       This Walt animation instance
+       */
+      'done': function(doneFnc) {
+        var anim = this;
+
+        return anim.after(doneFnc);
+      },
+
+      /**
+       * Add function to fire just after each animation item takes place
+       * (This is pretty much for animation a collection of items)
+       * 
+       * @param  {function}   onBeforeEach Function
+       * @return {Walt}       This Walt animation instance
+       */
       'afterEach': function(onAfterEachFnc) {
         var anim = this;
 
+        // gotta wrap it in a callback thing since we'll async it later
         anim.onAfterEaches.push(function(callback) {
           onAfterEachFnc && onAfterEachFnc(anim.settings.element, anim.settings);
           callback && callback(null, null);
@@ -186,12 +353,24 @@
       },
 
 
-      'done': function(doneFnc) {
-        var anim = this;
 
-        return anim.after(doneFnc);
-      },
 
+      // 
+      // 
+      // 
+      // Control functions
+      // 
+      // 
+      //
+
+
+
+      /**
+       * Sets the current animation state to 'paused'
+       * TODO: set in-progress animation state
+       * 
+       * @return {Walt}       This Walt animation instance
+       */
       'pause': function() {
         var anim = this;
 
@@ -200,6 +379,11 @@
         return anim;
       },
 
+      /**
+       * Plays the current animation (via state = 'running')
+       * 
+       * @return {Walt}       This Walt animation instance
+       */
       'play': function() {
         var anim = this;
 
@@ -208,6 +392,11 @@
         return anim;
       },
 
+      /**
+       * Resumes the current animation (via state = 'running')
+       * 
+       * @return {Walt}       This Walt animation instance
+       */
       'resume': function() {
         var anim = this;
 
@@ -217,9 +406,12 @@
       },
 
 
-
-      // functional methods
-
+      /**
+       * Runs all of the onBefore functions,
+       * then executes (begins) the animation
+       * 
+       * @return {Walt}       This Walt animation instance
+       */
       'animate': function() {
         var anim = this;
 
@@ -228,67 +420,116 @@
         return anim;
       },
 
-      '_createCssObj': function(cssString) {
+
+      /**
+       * Creates a new Walt instance with all of the current animation properties.
+       * Before/after events may be optionally cloned with a true/false paramater
+       * 
+       * @param  {boolean}  eventsToo   Should the before/after events be copied too?
+       * @return {Walt}                 New Walt animation instance
+       */
+      'fork': function(eventsToo) {
         var anim = this;
 
-        return {
-          '-webkit-animation': cssString || '',
-          'animation': cssString || ''
-        };
+        var newGuy = new Walt();
+        for (var prop in anim) {
+          if (!eventsToo && (prop === 'onBefores' || prop === 'onAfters' || prop === 'onBeforeEaches' || prop === 'onAfterEaches')) {
+            // ok
+          } else {
+            newGuy[prop] = anim[prop];
+          }
+        }
+
+        return newGuy;
       },
 
 
+      /**
+       * Internal function to begin the animation
+       * Creates the css shorthand string, and applies to element to kick off animations
+       * 
+       * @return {Walt}       This Walt animation instance
+       */
       '_executeAnim': function() {
         var anim = this;
         // modify the animation before we build the CSS string
-        anim.resume();
+        anim.play();
 
         var settings = anim.settings,
+          // create an animation shorthand string
           cssString = settings.animation + ' ' + settings.duration + ' ' + settings.timing + ' ' + settings.delay + ' ' + settings.count + ' ' + settings.direction + ' ' + settings.fill + ' ' + settings.state;
 
-        // execution = we're playing
 
+        // create a count of how many elements we should be watching out for
         anim.animCount = 0;
         anim.animMax = anim.settings.element.length;
 
-        anim.settings.element.on('animationend.walt', anim._onAnimEndEvent.bind(anim));
-        anim.settings.element.css(anim._createCssObj(cssString));
+        // bind the animation end handler
+        anim.settings.element.unbind('animationend.walt').on('animationend.walt', anim._onAnimEndEvent.bind(anim));
+        // trigger the animation!
+        anim.settings.element.css(_createCssObj(cssString));
+
+        return anim;
       },
 
+
+      /**
+       * Internal handler for 'animationend' events
+       * @param  {Event}      animationend Event object 
+       * @return {Walt}       This Walt animation instance
+       */
       '_onAnimEndEvent': function(event) {
         var anim = this,
           $target = $(event.currentTarget);
 
         $target.unbind('animationend.walt');
-        // reset the css by just passing in null values for the properties
-        $target.css(anim._createCssObj());
 
+        // reset the target css by just passing in null values for the properties
+        $target.css(_createCssObj());
+
+        // up the 'completed' count
         anim.animCount += 1;
 
         // fire afterEach for each one
-
         async.parallel(anim.onAfterEaches, function() {
-          // if we're done then we can fire the onAfters
+          // if we've finished animating all our items,
+          // then we can trigger the 'totally done' handler
           if (anim.animCount > anim.animMax) {
             anim._onAnimComplete();
           }
         });
+
+        return anim;
       },
 
+      /**
+       * 'Animation is totally done' event handler.
+       * Basically runs through the onAfters and resets css/vars where necessary.
+       * @return {Walt}       This Walt animation instance
+       */
       '_onAnimComplete': function() {
         var anim = this;
 
+        // run through the onAfter functions, then..
         async.parallel(anim.onAfters, function() {
+          // 'reset' the animation counters
           delete anim.animCount;
           delete anim.animMax;
 
-
           // reset the css by just passing in null values for the properties
-          anim.settings.element.css(anim._createCssObj());
+          anim.settings.element.css(_createCssObj());
         });
+
+        return anim;
       },
 
 
+      /**
+       * Easing definitions.
+       * To be used with the `timing` functin, e.g.:
+       *
+       * `...timing(Walt.prototype.easings.easeInQuad)...`
+       */
       'easings': {
         'linear': 'cubic-bezier(0.250, 0.250, 0.750, 0.750)',
         'ease': 'cubic-bezier(0.250, 0.100, 0.250, 1.000)',
